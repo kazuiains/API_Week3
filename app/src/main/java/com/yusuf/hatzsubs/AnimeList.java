23a981b4ac773591e47b45fa275f98d51a6ebe83
kazuiains <kaisaryusuf@gmail.com>
2018-05-03 13:27:13 +0700

    Initial commit

---
+++ app/src/main/java/com/yusuf/hatzsubs/AnimeList.java
@@ -0,0 +1,13 @@
+package com.yusuf.hatzsubs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnimeList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anime_list);
+    }
+}
